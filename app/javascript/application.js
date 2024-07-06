4eefd3c22f0df939e65bb104b08319e7f989a0c1
Raman99444 <pramachandran.mca@gmail.com>
2024-07-06 11:36:58 +0530

    attempt to fix validation problem with heroku

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,6 +2,7 @@ import Rails from "@rails/ujs"
 import { Turbo } from "@hotwired/turbo-rails"
 import "controllers"
 import "bootstrap"
+import '../assets/javascripts/booking_validation'
 
 Rails.start()
 Turbo.start()
